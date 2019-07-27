4a84cfd5b7c82ef8b1b184914966d5f876fd12d5
OfirEzra <45737748+OfirEzra@users.noreply.github.com>
2019-07-27 23:51:57 +0300

    Add files via upload

---
+++ BouncyBall/BouncyBall/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace BouncyBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BallPictureBox = new System.Windows.Forms.PictureBox();
+            this.ActionButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.BallPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BallPictureBox
+            // 
+            this.BallPictureBox.Image = global::BouncyBall.Properties.Resources.ball;
+            this.BallPictureBox.InitialImage = global::BouncyBall.Properties.Resources.ball;
+            this.BallPictureBox.Location = new System.Drawing.Point(353, 120);
+            this.BallPictureBox.Name = "BallPictureBox";
+            this.BallPictureBox.Size = new System.Drawing.Size(122, 118);
+            this.BallPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BallPictureBox.TabIndex = 0;
+            this.BallPictureBox.TabStop = false;
+            // 
+            // ActionButton
+            // 
+            this.ActionButton.Location = new System.Drawing.Point(348, 415);
+            this.ActionButton.Name = "ActionButton";
+            this.ActionButton.Size = new System.Drawing.Size(127, 23);
+            this.ActionButton.TabIndex = 1;
+            this.ActionButton.Text = "Start!";
+            this.ActionButton.UseVisualStyleBackColor = true;
+            this.ActionButton.Click += new System.EventHandler(this.ActionButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ActionButton);
+            this.Controls.Add(this.BallPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BallPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BallPictureBox;
+        private System.Windows.Forms.Button ActionButton;
+    }
+}
+
---
+++ BouncyBall/BouncyBall/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncyBall
+{
+    public partial class Form1 : Form
+    {
+        private Random r = new Random();
+        private static bool hasStarted = false;
+        private static bool changedDirection = false;
+        private static int dx = 0;
+        private static int dy = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = r.Next(0, 817);
+            int y = r.Next(0, 489);
+            if (BallPictureBox.Size.Width + x > 816 - BallPictureBox.Size.Width)
+            {
+                x = 816 - BallPictureBox.Size.Width - 16;
+            }
+            else if (-1 * BallPictureBox.Size.Width + x < 0)
+            {
+                x = BallPictureBox.Size.Width + 16;
+            }
+            if (BallPictureBox.Size.Height + y > 488 - BallPictureBox.Size.Height)
+            {
+                y = 488 - BallPictureBox.Size.Height - 32;
+            }
+            else if (-1 * BallPictureBox.Size.Height + y < 0)
+            {
+                y = BallPictureBox.Size.Height + 32;
+            }
+            BallPictureBox.Location = new Point(x, y);
+        }
+
+        private void ActionButton_Click(object sender, EventArgs e)
+        {
+            if (!hasStarted)
+            {
+                ActionButton.Text = "Change Direction";
+                DirectionSettings();
+                hasStarted = true;
+            }
+            else
+            {
+                changedDirection = true;
+            }
+            
+        }
+        private void DirectionSettings()
+        {
+            Task.Run(() =>
+            {
+                int direction = 5;
+                while (true)
+                {
+                    if (changedDirection == true)
+                    {
+                        direction = r.Next(1, 9);
+                        changedDirection = false;
+                        switch (direction)
+                        {
+                            case 1:
+                                dx = 0;
+                                dy = 1;
+                                break;
+                            case 2:
+                                dx = 1;
+                                dy = 1;
+                                break;
+                            case 3:
+                                dx = 1;
+                                dy = 0;
+                                break;
+                            case 4:
+                                dx = 1;
+                                dy = -1;
+                                break;
+                            case 5:
+                                dx = 0;
+                                dy = -1;
+                                break;
+                            case 6:
+                                dx = -1;
+                                dy = -1;
+                                break;
+                            case 7:
+                                dx = -1;
+                                dy = 0;
+                                break;
+                            case 8:
+                                dx = -1;
+                                dy = 1;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        MoveTheBall();
+                    }
+                    Thread.Sleep(2);
+                }
+            });
+        }
+        private void MoveTheBall()
+        {
+            if (BallPictureBox.Location.X + dx < 0)
+            {
+                dx = dx * -1;
+            }
+            else if (BallPictureBox.Location.X + BallPictureBox.Size.Width + 16  > 816)
+            {
+                dx = dx * -1;
+            }
+            if (BallPictureBox.Location.Y + dy < 0)
+            {
+                dy = dy * -1;
+            }
+            else if (BallPictureBox.Location.Y + BallPictureBox.Size.Height + 32 > 488)
+            {
+                dy = dy * -1;
+            }
+            Action<object, object> draw = (x, y) => { BallPictureBox.Location = new Point(BallPictureBox.Location.X + (int)x, BallPictureBox.Location.Y + (int)y); };
+            object[] coords = {dx ,dy};
+            BallPictureBox.BeginInvoke(draw, coords);
+            
+        }
+    }
+}
